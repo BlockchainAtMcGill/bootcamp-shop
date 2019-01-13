eadd846f202576879305cb9c1ead64011ad0b6fa
brendanfurtado <brenfurtado@gmail.com>
2019-01-12 21:29:37 -0500

    new contract

--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -1,12 +1,11 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 contract Shop {
     
     address owner;
     Item[] items;
     mapping (address => uint) public itemsOwned;
     mapping (address => uint) public balances; //Will represent the balances of a buyer
-    uint leftOver;
-  
+
     //What an object item possesses
     struct Item{
         string name; 
@@ -14,8 +13,7 @@ contract Shop {
         uint price; 
         uint quantity;
         address seller;
-        address owner;
-        //Add in other stuff like categories if needed later
+        address owner; 
     }
     
     //Basic modifier indicating that an owner can only use certain things 
@@ -24,16 +22,15 @@ contract Shop {
         _;
     }
     
-    constructor() public {
+    constructor() public payable{
         owner = msg.sender;
     }
   
-    function listItem(string name, uint price, uint quantity) public {
+    function listItem(string name, uint price, uint quantity) public onlyOwner {
         
-        uint unitPrice = 1 ether;
-        // items[ID] = Item(name, ID, price, quantity, msg.sender, 0);
         uint ID = items.push(Item(name, 0, price, quantity, msg.sender, 0)) - 1;
         items[ID].ID = ID;
+        
     }
     
     function deleteItem(uint ID) public onlyOwner{
@@ -42,7 +39,7 @@ contract Shop {
         require(item.ID >=0);
         
         if (items.length > 1) {
-            items[ID] = items[items.length-1];
+            items[ID] = items[items.length-1]; //Setting the last item to the index where the item was deleted
         }
         
         items.length--;
@@ -52,27 +49,30 @@ contract Shop {
         
         //Make sure the amount sent to buy item is correct
         Item memory item = items[ID];
+        
         require(msg.value >= item.price);
-        itemsOwned[msg.sender] = ID;
-        items[ID].owner = msg.sender;
-        balances[item.seller] += msg.value;
-        leftOver += msg.value - item.price;
+        require(items[ID].quantity > 0);
+        
+        itemsOwned[msg.sender] = ID; //Add new item in my own list
+        items[ID].owner = msg.sender; //Make the item be the guy who sent the order
+        balances[item.seller] += msg.value; //Increment seller's balance
+        balances[item.owner] -= msg.value;  //Decrement buyer's balance
+        items[ID].quantity -= 1;
+      	
     }
   
-    //Withdraw function
+    //Withdraw function, whatever is in the balances, 
     function withdrawFunds() public {
         
-        address payee = msg.sender;
+        address payee = msg.sender; 
         uint payment = balances[payee];
+        
         //Check if payment is positive
         require(payment > 0);
         balances[payee] = 0;
+        
         //Move funds
         require(payee.send(payment));
-    }
-  
-    function withdrawLeftover() public onlyOwner {
-        require(msg.sender.send(leftOver));
     }
   
     //Retrieve balance of a seller
@@ -85,4 +85,5 @@ contract Shop {
         Item memory item = items[ID];
         return (item.ID, item.name, item.price, item.quantity);
     }
+    
 }
