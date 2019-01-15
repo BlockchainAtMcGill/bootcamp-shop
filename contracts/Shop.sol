84ee8387bca175b5dc9031a075eac6d80673918a
dliuproduction <dliuproduction@gmail.com>
2019-01-14 22:42:51 -0500

    Update contract and web3 calls

--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -3,18 +3,10 @@ pragma solidity ^0.4.24;
 contract Shop {
     
     address public contractOwner;
-    Item[] public items;
+    address[] public itemOwners;
+    uint[] public itemPrices;
     mapping (address => uint[]) public itemsOwned;
     mapping (address => uint) public balances; //Will represent the balances of a buyer
-
-    //What an object item possesses
-    struct Item {
-        string name; 
-        uint ID;
-        uint price;
-        address owner;
-        bool active;
-    }
     
     //Basic modifier indicating that an owner can only use certain things 
     modifier onlyOwner() {
@@ -26,33 +18,30 @@ contract Shop {
         contractOwner = msg.sender;
     }
   
-    function listItem(string name, uint price) public onlyOwner {
-        uint ID = items.push(Item(name, 0, price * 1 ether, msg.sender, true)) - 1;
-        items[ID].ID = ID;
+    function listItem(uint price) public onlyOwner {
+        itemPrices.push(price * 1 ether) - 1;
+        itemOwners.push(msg.sender);
     }
     
     function deleteItem(uint ID) public {
-
-        require(msg.sender == items[ID].owner);
-        delete items[ID];
+        require(itemOwners[ID] == msg.sender);
+        delete itemOwners[ID];
+        delete itemPrices[ID];
     }
     
     function buyItem(uint ID) public payable {
+        address previousOwner = itemOwners[ID];
+        require(previousOwner != address(0));
+        require(previousOwner != msg.sender);
+        require(itemPrices[ID] == msg.value);
         
-        Item memory item = items[ID];
-        
-        require(item.active); //Must be active item
-        require(item.owner != msg.sender); //Prevent buying own item
-        require(msg.value == item.price); //Must send exact amount
-        
-        balances[item.owner] += msg.value; //Increment seller's balance
+        balances[previousOwner] += msg.value; //Increment seller's balance
         itemsOwned[msg.sender].push(ID); //Add new item in my own list
-        items[ID].owner = msg.sender; //Make the owner be the guy who sent the order
+        itemOwners[ID] = msg.sender; //Make the owner be the guy who sent the order
     }
   
     //Withdraw function, whatever is in the balances, 
     function withdrawFunds() public {
-        
         address payee = msg.sender; 
         uint payment = balances[payee];
         
@@ -62,5 +51,20 @@ contract Shop {
         
         //Move funds
         require(payee.send(payment));
+    }
+
+    //Retrieve prices
+    function getItemOwners() public view returns (address[] memory) {
+        return itemOwners;
+    }
+    
+    //Retrieve owners
+    function getItemPrices() public view returns (uint[] memory) {
+        return itemPrices;
+    }
+
+    //Retrieve balance of a seller
+    function balanceOf(address seller) public view returns (uint) {
+        return balances[seller];
     }
 }
