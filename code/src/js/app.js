20b99cdafd39145f90743e3ac4171c09739cb293
dliuproduction <dliuproduction@gmail.com>
2019-01-19 16:43:26 -0500

    List and withdraw working

--- code/src/js/app.js
+++ code/src/js/app.js
@@ -1,50 +1,59 @@
 App = {
   web3Provider: null,
   contracts: {},
+  instance: null,
 
   init: function() {
     return App.initWeb3();
   },
 
-  getItems: function() {
-      
-      var shopInstance;
-      var itemCount;
-      var itemPromises = [];
-      var items = [];
+  initFunctions: function() {
 
-      App.getBalance();
+    shopInstance = App.instance;
 
-      App.contracts.Shop.deployed().then(function(instance) {
-        shopInstance = instance;
-        return shopInstance.itemCount.call();
-      }).then(function(data) {
-        itemCount = data.toString();
-        console.log(`Number of items: ${itemCount}`);
-        for (var i = 0; i < itemCount; i++) {
-          itemPromises.push(shopInstance.items.call(i))
-        }
-        Promise.all(itemPromises).then(contractData => {
-          // Load item images
-          $.getJSON('../images.json').then(localData => {
-            contractData.map(data => {
-              var item = {};
-              item.name = data[0];
-              item.id = parseInt(data[1].toString());
-              item.price = parseInt(data[2].toString());
-              item.owner = data[3];
-              localData.hasOwnProperty(item.id) ?
-              item.image = localData[item.id].image :
-              item.image = "images/default.png";
-              items.push(item);
-            });
-            console.log(items);
-            App.renderItems(items);
+    var itemCount;
+    var itemPromises = [];
+    var items = [];
+
+    var listItemEvent = shopInstance.listItemEvent();
+    var deleteItemEvent = shopInstance.deleteItemEvent();
+    var buyEvent = shopInstance.buyEvent();
+    var withdrawFundsEvent = shopInstance.withdrawFundsEvent();
+    
+    listItemEvent.watch(function(error, result){});
+    deleteItemEvent.watch(function(error, result){});
+    buyEvent.watch(function(error, result){});
+    withdrawFundsEvent.watch(function(error, result){});  
+
+    App.getBalance();
+
+    shopInstance.itemCount.call().then(function(data) {
+      itemCount = data.toString();
+      console.log(`Number of items: ${itemCount}`);
+      for (var i = 0; i < itemCount; i++) {
+        itemPromises.push(shopInstance.items.call(i))
+      }
+      Promise.all(itemPromises).then(contractData => {
+        // Load item images
+        $.getJSON('../images.json').then(localData => {
+          contractData.map(data => {
+            var item = {};
+            item.name = data[0];
+            item.id = parseInt(data[1].toString());
+            item.price = parseInt(data[2].toString());
+            item.owner = data[3];
+            localData.hasOwnProperty(item.id) ?
+            item.image = localData[item.id].image :
+            item.image = "images/default.png";
+            items.push(item);
           });
+          console.log(items);
+          App.renderItems(items);
         });
-      }).catch(function(err) {
-        console.log(err.message);
       });
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   renderItems: function(items) {
@@ -54,8 +63,10 @@ App = {
     for (i = 0; i < items.length; i ++) {
       items[i].id = i;
       itemTemplate.find('.panel-title').text(items[i].name);
+      itemTemplate.find('.item-title').text(items[i].name);
       itemTemplate.find('img').attr('src', items[i].image);
-      itemTemplate.find('.item-price').text(items[i].price);
+      itemTemplate.find('.item-id').text(items[i].id);
+      itemTemplate.find('.item-price').text(parseInt(items[i].price) / 1000000000000000000);
       itemTemplate.find('.item-owner').text(items[i].owner);
       itemTemplate.find('.btn-buy').attr({'data-id': items[i].id, 'data-price': items[i].price});
       itemTemplate.find('.btn-list').attr({'data-id': items[i].id, 'data-price': items[i].price});
@@ -73,7 +84,6 @@ App = {
       App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
       web3 = new Web3(App.web3Provider);
     }
-
 
     var networkName;
 
@@ -106,27 +116,29 @@ App = {
       // Set the provider for our contract.
       App.contracts.Shop.setProvider(App.web3Provider);
 
+      // Get the deployed contract on local blockchain
+      App.contracts.Shop.deployed().then(function(shopInstance) {
+        App.instance = shopInstance;
+        App.initFunctions();
+        return App.bindEvents();
+      })
       // Use our contract to retieve and mark the bought items.
-      return App.getItems();
     });
-
-    return App.bindEvents();
   },
 
   bindEvents: function() {
     $(document).on('click', '.btn-buy', App.handleBuy);
-    $(document).on('click', '.btn-list', App.handleList);
+    $(document).on('submit', '.form-post', App.handleList);
     $(document).on('click', '.btn-delete', App.handleDelete);
     $(document).on('click', '.btn-withdraw', App.handleWithdraw);
   },
 
-  handleList: function() {
-
-    event.preventDefault();
-
-    var itemPrice = parseInt($(event.target).data('price'));
-
-    console.log(`listing item for ${itemPrice} ETH`);
+  handleList: event => {
+    var name = event.target.name.value;
+    var price = event.target.price.value;
+    // var image = `images/${event.target.image.value.replace(/.*[\/\\]/, '')}`;
+    // console.log(image);
+    console.log(`listing item ${name} for ${price} ETH`);
 
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
@@ -135,11 +147,8 @@ App = {
 
       var account = accounts[0];
 
-      App.contracts.Shop.deployed().then(function(instance) {
-        shopInstance = instance;
-
-        return shopInstance.listItem(itemPrice * 1000000000000000000, {from: account, gas: 999999});
-      }).then(function(result) {
+      shopInstance = App.instance;
+      shopInstance.listItem(name, price * 1000000000000000000, {from: account, gas: 999999}).then(function(result) {
         console.log(result);
       }).catch(function(err) {
         console.log(err.message);
@@ -157,11 +166,8 @@ App = {
 
       var account = accounts[0];
 
-      App.contracts.Shop.deployed().then(function(instance) {
-        shopInstance = instance;
-
-        return shopInstance.withdrawFunds({from: account, gas: 999999});
-      }).then(function(result) {
+      shopInstance = App.instance;
+      shopInstance.withdrawFunds({from: account, gas: 999999}).then(function(result) {
         // console.log(`account balance: ${web3.fromWei(web3.eth.getBalance(account))}`);
       }).catch(function(err) {
         console.log(err.message);
@@ -183,12 +189,9 @@ App = {
 
       var account = accounts[0];
 
-      App.contracts.Shop.deployed().then(function(instance) {
-        shopInstance = instance;
-
-        return shopInstance.deleteItem(itemId, {from: account, gas: 999999});
-      }).then(function(result) {
-        return App.getItems();
+      shopInstance = instance;
+      shopInstance.deleteItem(itemId, {from: account, gas: 999999}).then(function(result) {
+        return App.initFunctions();
       }).catch(function(err) {
         console.log(err.message);
       });
@@ -211,13 +214,9 @@ App = {
 
       var account = accounts[0];
 
-      App.contracts.Shop.deployed().then(function(instance) {
-        shopInstance = instance;
-
-        return shopInstance.buyItem(itemId, {from: account, value: itemPrice * 1000000000000000000, gas: 999999});
-      }).then(function(result) {
-        return App.getItems();
-        
+      shopInstance = App.instance;
+      shopInstance.buyItem(itemId, {from: account, value: itemPrice, gas: 999999}).then(function(result) {
+        // return App.initFunctions();
       }).catch(function(err) {
         console.log(err.message);
       });
@@ -240,12 +239,9 @@ App = {
       $('#wallet-address').text(account);
       var shopInstance;
 
-      App.contracts.Shop.deployed().then(function(instance) {
-        shopInstance = instance;
-        console.log(accounts)
-        return shopInstance.balances.call(account, {from: account});
-      }).then(balance => {
-        $('#balance-amount').text(balance.toString());
+      shopInstance = App.instance;
+      shopInstance.balances.call(account, {from: account}).then(balance => {
+      $('#balance-amount').text(`${parseInt(balance.toString()) / 1000000000000000000} ETH`);
       }).catch(err => {
         console.log(err.message);
       });
